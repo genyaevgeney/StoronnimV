ff5c4b1856aed0b9b8cdafa39e7cbb26263b5ae7
AlessioJorgevich <AlessioJorgevich@users.noreply.github.com>
2019-07-09 06:21:39 +0000

    routes/web.php
    added routes for news and feedbacks

--- app/routes/web.php
+++ app/routes/web.php
@@ -11,11 +11,50 @@
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::view('/test', 'test');//This route is for dev purposes only. To be deleted.
 
+/*
+* This is frontpage route.
+* Should be kept outside Auth group.
+*/
+Route::get('/', 'IndexController@index');
 
+/*
+* This route is for submitting user's request from the frontpage.
+* Should be kept outside Auth group.
+*/
+Route::post('/feedback', 'FeedbackController@store')->name('saveMessage');
+
+/*
+* This expression disables registration option to admin panel.
+*
+*/
 Auth::routes(['register' => false]);
 
+/*
+* This route is for homepage of the admin panel.
+* This route is ouside of Auth group due to Auth middleware set at HomeController constructor.
+*
+*/
 Route::get('/home', 'HomeController@index')->name('home');
+
+/*
+* This is Auth group.
+* All routes within this group are accessible only for authorized users.
+*
+*/
+Route::group(['middleware' => ['auth']], function () {
+
+    Route::prefix('/news')->group(function () {
+        Route::get('/', 'NewsController@index');
+        Route::get('/create', 'NewsController@create');
+        Route::get('/archive', 'NewsController@archive');
+        Route::post('/store', 'NewsController@store');
+        // Route::post('/avatar', 'UserController@updateAvatar');
+        // Route::post('/information', 'UserController@editInformation');
+        // Route::post('/password', 'UserController@editPassword');
+        // Route::get('/set-lang/{lang}/{route}', 'UserController@setLang');
+    });
+
+
+});
