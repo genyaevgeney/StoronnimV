3994133fe70336302707b54422393e37ecfcaa71
AlessioJorgevich <AlessioJorgevich@users.noreply.github.com>
2019-08-22 13:34:47 +0000

    ajax.content.js updated
    one line of code split into several ones

--- app/public/js/ajax-content.js
+++ app/public/js/ajax-content.js
@@ -21,7 +21,29 @@ $(document).ready(function(){
 						var date = moment(data.updated_at).locale('uk');
 						var month = date.format('MMMM');
 						var day = date.date();
-						var newsSection = "<!-- news --><div class=\"col-md-10 col-lg-6 StoronnimV-news-section__news\"><div class=\"StoronnimV-news-section__news-content\"><img src=\"storage/images/"+ data.title_image +"\" alt=\"" + data.updated_at + "\" class=\"StoronnimV-news-section__news-image\"><div class=\"StoronnimV-news-section__text-div\"><div class=\"StoronnimV-news-section__wrap-main-text\"><a href=\"#\" class=\"StoronnimV-news-section__heading\">" + data.title + "</a><p class=\"StoronnimV-news-section__description\">" + data.content + "</p></div><div class=\"StoronnimV-news-section__news-footer\"><a href=\"#\" class=\"StoronnimV-news-section__like\">120</a><a href=\"#\" class=\"StoronnimV-news-section__read-more\">Дізнатися більше</a></div></div></div><div class=\"StoronnimV-news-section__news-date-div\"><span class=\"StoronnimV-news-section__month\">"+ month +"</span><span class=\"StoronnimV-news-section__day\">"+ day +"</span></div></div><!-- news -->";
+						var newsSection = `
+						<!-- a piece of news -->
+						<div class=\"col-md-10 col-lg-6 StoronnimV-news-section__news\">
+							<div class=\"StoronnimV-news-section__news-content\">
+								<img src=\"storage/images/`+ data.title_image +`\" alt=\"` + data.title + `\" class=\"StoronnimV-news-section__news-image\">
+								<div class=\"StoronnimV-news-section__text-div\">
+									<div class=\"StoronnimV-news-section__wrap-main-text\">
+										<a href=\"#\" class=\"StoronnimV-news-section__heading\">` + data.title + `</a>
+										<p class=\"StoronnimV-news-section__description\">` + data.content + `</p>
+									</div>
+									<div class=\"StoronnimV-news-section__news-footer\">
+										<a href=\"#\" class=\"StoronnimV-news-section__like\"> 120 </a>
+										<a href=\"#\" class=\"StoronnimV-news-section__read-more\">Дізнатися більше</a>
+									</div>
+								</div>
+							</div>
+						
+							<div class=\"StoronnimV-news-section__news-date-div\">
+								<span class=\"StoronnimV-news-section__month\">`+ month +`</span>
+								<span class=\"StoronnimV-news-section__day\">`+ day +`</span>
+							</div>
+						</div>
+						<!-- end of a piece of news -->`;
 						$(".row").append(newsSection);
 					});
 					inProgress = false;
