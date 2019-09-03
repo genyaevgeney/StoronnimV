44be5409342ee64a42196283b7014cbf7af374b5
AlessioJorgevich <AlessioJorgevich@users.noreply.github.com>
2019-09-03 04:51:13 +0000

    ImageController created

---
+++ app/app/Http/Controllers/ImageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+    public function compressImage($value='')
+    {
+    	# code...
+    }
+
+    public function cropImage($value='')
+    {
+    	# code...
+    }
+}
